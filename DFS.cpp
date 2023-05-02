77172f25bac164de71a21193bb1ccc1f7f1a86bd
Ayush_S_Acharya <115517887+AyushAcharya07@users.noreply.github.com>
2023-05-02 20:03:36 +0530

    Update DFS.cpp

--- DFS.cpp
+++ DFS.cpp
@@ -1,99 +1,137 @@
-# include <iostream>
-# include <string.h>
+#include <iostream>
+#include <string.h>
 using namespace std;
 
 int n;
 int a_matrix[10][10];
-int stk[20],v_array[20];
+int visited[10],stk[20];;
 
-class adj_mat
+
+class Stack
 {
     public:
-    void entry()
+    int top=-1;
+
+    bool isEmpty() 
     {
-        cout<<"Enter the no. of vertices of the graph : "<<endl;
+        return top==-1;
+    }
+
+    void push(int value) 
+    {
+        stk[++top]=value;
+    }
+
+    int pop() 
+    {
+        return stk[top--];
+    }
+};
+
+class Graph 
+{
+    public:
+    void entry() 
+    {
+        cout<<"Enter the no. of vertices of the graph: "<<endl;
         cin>>n;
-        cout<<"Enter 1 if there is an edge present between the 2 vertices : "<<endl;
+        cout<<"Enter 1 if there is an edge present between the 2 vertices, 0 otherwise: "<<endl;
         for (int i=0;i<n;i++)
         {
-            for(int j=0;j<n;j++)
+            for (int j=0;j<n;j++)
             {
-                cin>>a_matrix[i][j];
+                int val;
+                cout<<"Is there an edge between vertex "<<i<<" and vertex "<<j<<"? (0/1): ";
+                cin>>val;
+                a_matrix[i][j]=val;
             }
         }
 
-        cout<<"The adjacency matrix is :"<<endl;
-        cout<<"* "<<n-n<<" "<<n-2<<" "<<n-1<<endl;
+        cout<<"The adjacency matrix is: "<<endl;
+        cout<<"* ";
+        for (int i=0;i<n;i++)
+        {
+            cout<<i<<" ";
+        }
+        cout<<endl;
 
         for (int i=0;i<n;i++)
         {
             cout<<i<<" ";
             for (int j=0;j<n;j++)
             {
-                cout<<a_matrix[i][j]<<" ";
+                cout <<a_matrix[i][j]<<" ";
             }
             cout<<endl;
         }
     }
-};
 
-class stack
-{
-    public:
-    int top;
-    stack()
+    void DFS(int v) 
     {
-        top=-1;
-    }  
+        Stack s;
+        s.push(v);
+        visited[v]=1;
+        cout<<v<<" ";
 
-    int isempty()
-    {
-        if (top==-1)
-            return 1;
-        else
-            return 0;
-    }
-
-    int isfull()
-    {
-        if (top==n)
-            return 1;
-        else
-            return 0;
-    }
-
-    void push(int value)
-    {
-        if (isfull()==1)
-            cout<<"Stack is full"<<endl;
-        else
+        while (!s.isEmpty()) 
         {
-            top++;
-            stk[top]=value;
+            int w=-1;
+            int u=s.pop();
+
+            for (int i=0;i<n;i++) 
+            {
+                if (a_matrix[u][i]==1 && visited[i]==0) 
+                {
+                    w=i;
+                    break;
+                }
+            }
+
+            if (w!=-1) 
+            {
+                visited[w]=1;
+                cout<<w<<" ";
+                s.push(u);
+                s.push(w);
+            }
         }
     }
 
-    void pop()
+    /*void printVisited() 
     {
-        int value;
-        if (isempty()==1)
-            cout<<"Stack is empty"<<endl;
-        else
+        cout <<"Visited array : ";
+        for (int i=0;i<n;i++) 
         {
-            value=stk[top];
-            top--;
+            cout<<visited[i]<<" ";
+        }
+        cout << endl;
+    }*/
+    void printVisited() 
+    {
+    cout <<"Visited vertices: ";
+    for (int i=0;i<n;i++) 
+    {
+        if (visited[i]==1)
+        {
+            cout<<i<<" ";
         }
     }
+    cout << endl;
+    }
+
 };
 
-class DFS
+int main() 
 {
-    public:
-    adj_mat a;
-    stack s;
-    void convert()
-    {
-        a.entry();
-        int i=0;
-    }
-};
+    Graph g;
+    g.entry();
+    memset(visited, 0, sizeof(visited));
+
+    cout << "Starting DFS from vertex 0: ";
+    g.DFS(0);
+    cout << endl;
+    cout<<"done"<<endl;
+    g.printVisited();
+
+    return 0;
+}
