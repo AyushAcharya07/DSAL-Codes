61fe8c8e8db7092faac4a6a73d06824ac82ee7ca
Ayush_S_Acharya <115517887+AyushAcharya07@users.noreply.github.com>
2023-05-03 20:09:39 +0530

    Update prims.cpp

--- prims.cpp
+++ prims.cpp
@@ -38,7 +38,7 @@ void Tree::tree_create()
     cout<<"Contents of visited array before traversal are : "<<endl;
     for (int i=0;i<v;i++)
     {
-        cout<<v_array[i]<<" ";
+        cout<<v_array[i]<<"  ";
     }cout<<endl;
 
     for (int i=0;i<v;i++)   // initialization of adjacency matrix with 999's
@@ -54,7 +54,7 @@ void Tree::tree_create()
     {
         for (int j=0;j<v;j++)
         {
-            cout<<arr[i][j]<<" ";
+            cout<<arr[i][j]<<"  ";
         }cout<<endl;
     }
 
@@ -76,7 +76,7 @@ void Tree::tree_create()
     {
         for (int j=0;j<v;j++)
         {
-            cout<<arr[i][j]<<" ";
+            cout<<arr[i][j]<<"\t";
         }cout<<endl;
     }
 }
@@ -120,25 +120,64 @@ void Tree::min_path()
 int main()
 {
     Tree t;
-    t.tree_create();
-    t.min_path();
+    int ch,ans;
+    do
+    {
+        cout<<"**********MENU**********"<<endl;
+        cout<<"1.Create the tree of Phone lines\n2.Find the Minimum Spanning Tree (MST)\n3.Exit"<<endl;
+        cout<<"Enter your choice : "<<endl;
+        cin>>ch;
+        switch(ch)
+        {
+            case 1:
+                t.tree_create();
+                break;
+
+            case 2:
+                t.min_path();
+                break;
+
+            case 3:
+                cout<<"Exited successfully from the program!!"<<endl;
+                exit(0);
+                break;
+
+            default:
+                cout<<"You have entered a wrong choice\nPlease try again!!"<<endl;
+                break;
+        }
+        cout<<"\nDo you want to continue?\n1.Yes\n2.No"<<endl;
+        cin>>ans;
+    }while (ans==1);
+
+    if (ans==2)
+        cout<<"Exited successfully!!"<<endl;
+    
+    
     return 0;
 }
 
 Output:-
-    PS D:\Ayush\Codes\C++ Codes> cd "d:\Ayush\Codes\C++ Codes\" ; if ($?) { g++ prims.cpp -o prims } ; if ($?) { .\prims }
+
+PS D:\Ayush\Codes\C++ Codes> cd "d:\Ayush\Codes\C++ Codes\" ; if ($?) { g++ prims.cpp -o prims } ; if ($?) { .\prims }
+**********MENU**********
+1.Create the tree of Phone lines
+2.Find the Minimum Spanning Tree (MST)
+3.Exit
+Enter your choice : 
+1
 Enter the no. of vertices : 
 4
 Enter the no. of edges : 
 5
 Contents of visited array before traversal are : 
-0 0 0 0 
-Adjacency Matrix initialization : 
-999 999 999 999 
-999 999 999 999 
-999 999 999 999 
-999 999 999 999 
-Enter the vertices present at end point of edges :
+0  0  0  0
+Adjacency Matrix initialization :
+999  999  999  999
+999  999  999  999
+999  999  999  999
+999  999  999  999
+Enter the vertices present at end point of edges : 
 0
 1
 Enter the weight present between 0 and 1 :
@@ -164,11 +203,34 @@ Enter the vertices present at end point of edges :
 Enter the weight present between 2 and 3 :
 20
 Adjacency Matrix after updation is :
-999 5 8 999
-5 999 10 15
-8 10 999 20
-999 15 20 999
+999     5       8       999
+5       999     10      15
+8       10      999     20
+999     15      20      999
+
+Do you want to continue?
+1.Yes
+2.No
+1
+**********MENU**********
+1.Create the tree of Phone lines
+2.Find the Minimum Spanning Tree (MST)
+3.Exit
+Enter your choice :
+2
 The minimum path found is 0->1 with cost : 5
 The minimum path found is 0->2 with cost : 8
 The minimum path found is 1->3 with cost : 15
 The minimum cost after traversal using Prim's algorithm is : 28
+
+Do you want to continue?
+1.Yes
+2.No
+1
+**********MENU**********
+1.Create the tree of Phone lines
+2.Find the Minimum Spanning Tree (MST)
+3.Exit
+Enter your choice :
+3
+Exited successfully from the program!!
